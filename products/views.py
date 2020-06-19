3dd7c4de56f0f782eb697b7a94f00f555dc1d73c
Pawan Lamba <pawan88.lamba@gmail.com>
2020-06-18 23:17:38 -0400

    Voting for the product

--- products/views.py
+++ products/views.py
@@ -28,7 +28,15 @@ def create(request):
     else:
         return render(request, 'products/create.html')
 
-
-def detail(request,product_id):
+@login_required
+def detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     return render(request, 'products/detail.html', {'product': product})
+
+
+def upvote(request, product_id):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, pk=product_id)
+        product.votes_total +=1
+        product.save()
+        return redirect('/products/' + str(product.id))
